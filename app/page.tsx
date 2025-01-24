89038851653c399dc0df9cdfe4f6694e260a7c58
Tom-Jose-Biju <tomjosebiju@gmail.com>
2025-01-24 22:30:15 +0530

    8th commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import { motion } from 'framer-motion'
 import { HeroSection } from '@/components/sections/Hero'
 import { AboutSectionHome } from '@/components/sections/About'
-import { EducationSection } from '@/components/sections/Education/index'
+import { EducationSection } from '@/components/sections/Education'
 import { ProjectsSection } from '@/components/sections/Projects'
 import { ContactSection } from '@/components/sections/Contact'
 
--- next.config.js
+++ next.config.js
@@ -2,11 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
   compiler: {
     removeConsole: process.env.NODE_ENV === "production",
   },
   images: {
     domains: ['images.unsplash.com'],
+    unoptimized: true,
   },
 }
 
